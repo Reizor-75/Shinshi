ae32a81d8c3450891ab25dc708c2ca734b0fb460
mlinares <m.linares93@gmail.com>
2024-01-25 13:28:41 -0500

    clean up routes

--- routes/animes.js
+++ routes/animes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import * as animesCtrl from "../controllers/animes.js";
 import { isAdmin, isLoggedIn } from "../middleware/middleware.js";
 
-const router = Router()
+const router = Router();
 
 // GET http://localhost:3000/
 router.get('/', animesCtrl.index)
@@ -12,11 +12,11 @@ router.get('/catalog', animesCtrl.displayCatalog)
 router.get('/new',isAdmin, animesCtrl.new)
 // GET http://localhost:3000/catalog/:animeId
 router.get('/catalog/:animeId', animesCtrl.show)
-// POST http://localhost:3000/catalog
-router.post('/catalog', isAdmin, animesCtrl.create)
+// POST http://localhost:3000/
+router.post('/', isAdmin, animesCtrl.create)
 // POST http://localhost:3000/catalog/:animeId/reviews
 router.post('/catalog/:animeId/reviews', isLoggedIn, animesCtrl.createReview)
-// PUT localhost:3000/catalog/:animeId/reviews/:reviewId
+// PUT localhost:3000/catalog/:animeId
 router.put('/catalog/:animeId', isAdmin, animesCtrl.update)
 // PUT localhost:3000/catalog/:animeId/reviews/:reviewId
 router.put('/catalog/:animeId/reviews/:reviewId', isLoggedIn, animesCtrl.updateReview)
--- routes/profiles.js
+++ routes/profiles.js
@@ -1,8 +1,8 @@
-import { Router } from 'express'
-import * as profilesCtrl from '../controllers/profiles.js'
+import { Router } from 'express';
+import * as profilesCtrl from '../controllers/profiles.js';
 import { isLoggedIn } from "../middleware/middleware.js";
 
-const router = Router()
+const router = Router();
 
 // GET localhost:3000/profiles
 router.get('/', isLoggedIn, profilesCtrl.index)
@@ -11,7 +11,7 @@ router.get('/watched', isLoggedIn, profilesCtrl.watchList)
 // GET localhost:3000/profiles/reviews
 router.get('/reviews', isLoggedIn, profilesCtrl.reviews)
 // POST http://localhost:3000/profiles/watched
-router.post('/watched', profilesCtrl.addWatchList)
+router.post('/watched', isLoggedIn, profilesCtrl.addWatchList)
 // PUT localhost:3000/profiles/reviews/:animeId/:reviewId
 router.put('/reviews/:animeId/:reviewId', isLoggedIn, profilesCtrl.updateReview)
 // DELETE localhost:3000/profiles/watched/:animeId
